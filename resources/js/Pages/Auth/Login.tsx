6b2496b9055c7334294d15c005e2f5f67d09cb60
Fth87 <fatihfwz87@gmail.com>
2024-11-10 11:42:10 +0700

    feat:slicing login and register page

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,11 +1,11 @@
 import Checkbox from '@/Components/Checkbox'
-import InputError from '@/Components/InputError'
-import InputLabel from '@/Components/InputLabel'
-import PrimaryButton from '@/Components/PrimaryButton'
-import TextInput from '@/Components/TextInput'
 import GuestLayout from '@/Layouts/GuestLayout'
 import { Head, Link, useForm } from '@inertiajs/react'
-import { FormEventHandler } from 'react'
+import { FormEventHandler, useState } from 'react'
+import { Button, Input } from '@nextui-org/react'
+import { EyeFilledIcon } from '@/Components/EyeFilledIcon'
+import { EyeSlashFilledIcon } from '@/Components/EyeSlashFilledIcon'
+import { FaGoogle } from 'react-icons/fa'
 
 export default function Login({
   status,
@@ -28,6 +28,10 @@ export default function Login({
     })
   }
 
+  const [isVisible, setIsVisible] = useState(false)
+
+  const toggleVisibility = () => setIsVisible(!isVisible)
+
   return (
     <GuestLayout>
       <Head title='Log in' />
@@ -36,66 +40,135 @@ export default function Login({
         <div className='mb-4 text-sm font-medium text-green-600'>{status}</div>
       )}
 
-      <form onSubmit={submit}>
-        <div>
-          <InputLabel htmlFor='email' value='Email' />
+      <div className='text-3xl font-semibold leading-10 text-[#02092f]'>
+        Login
+      </div>
 
-          <TextInput
-            id='email'
-            type='email'
+      <form onSubmit={submit}>
+        <div className='mt-10'>
+          <Input
+            errorMessage={errors.email}
             name='email'
             value={data.email}
-            className='mt-1 block w-full'
-            autoComplete='username'
-            isFocused={true}
             onChange={(e) => setData('email', e.target.value)}
+            isInvalid={!!errors.email}
+            isRequired
+            type='email'
+            label='Email'
+            labelPlacement='outside'
+            placeholder='Enter your email'
+            variant='flat'
+            classNames={{
+              label: 'text-black/50 dark:text-white/90',
+              input: [
+                'bg-transparent',
+                'text-black/90 ',
+                'placeholder:text-neutral-500 ',
+                'rounded-xl',
+                'focus:ring-0',
+                'active:ring-0',
+              ],
+              innerWrapper: ['bg-transparent', 'border-none', 'p-0'],
+              inputWrapper: ['p-0', 'border-none'],
+            }}
+            style={{ border: 'none' }}
           />
-
-          <InputError message={errors.email} className='mt-2' />
         </div>
-
-        <div className='mt-4'>
-          <InputLabel htmlFor='password' value='Password' />
-
-          <TextInput
-            id='password'
-            type='password'
+        <div className='mt-10'>
+          <Input
+            errorMessage={errors.password}
             name='password'
             value={data.password}
-            className='mt-1 block w-full'
-            autoComplete='current-password'
             onChange={(e) => setData('password', e.target.value)}
+            isInvalid={!!errors.password}
+            isRequired
+            label='password'
+            labelPlacement='outside'
+            placeholder='Enter your password'
+            radius='lg'
+            variant='flat'
+            classNames={{
+              label: 'text-black/50 dark:text-white/90',
+              input: [
+                'bg-transparent',
+                'text-black/90 ',
+                'placeholder:text-neutral-500 ',
+                'rounded-xl',
+                'focus:ring-0',
+                'active:ring-0',
+              ],
+              innerWrapper: ['bg-transparent', 'border-none', 'p-0'],
+              inputWrapper: ['p-0', 'border-none'],
+            }}
+            endContent={
+              <button
+                className='mr-2 focus:outline-none'
+                type='button'
+                onClick={toggleVisibility}
+                aria-label='toggle password visibility'
+              >
+                {isVisible ? (
+                  <EyeSlashFilledIcon className='pointer-events-none text-2xl text-default-400' />
+                ) : (
+                  <EyeFilledIcon className='pointer-events-none text-2xl text-default-400' />
+                )}
+              </button>
+            }
+            type={isVisible ? 'text' : 'password'}
+            style={{ border: 'none' }}
           />
-
-          <InputError message={errors.password} className='mt-2' />
         </div>
 
-        <div className='mt-4 block'>
-          <label className='flex items-center'>
-            <Checkbox
-              name='remember'
-              checked={data.remember}
-              onChange={(e) => setData('remember', e.target.checked)}
-            />
-            <span className='ms-2 text-sm text-gray-600 dark:text-gray-400'>
-              Remember me
+        <div className='mt-4 flex flex-col items-center'>
+          <div className='mb-3 flex w-full justify-between'>
+            <div className='mb-3 block self-start'>
+              <label className='flex items-center'>
+                <Checkbox
+                  name='remember'
+                  checked={data.remember}
+                  onChange={(e) => setData('remember', e.target.checked)}
+                />
+                <span className='ms-2 text-sm text-gray-600 dark:text-gray-400'>
+                  Remember me
+                </span>
+              </label>
+            </div>
+            {canResetPassword && (
+              <Link
+                href={route('password.request')}
+                className='mb-3 rounded-md text-sm text-gray-600 underline hover:text-gray-900'
+              >
+                Forgot your password?
+              </Link>
+            )}
+          </div>
+
+          <Button
+            className='mb-2 ms-4 w-full bg-gradient-to-r from-[#5451f2] to-[#9c7dfc] text-lg font-medium leading-7 text-blue-50'
+            isLoading={processing}
+            onClick={submit}
+          >
+            Masuk
+          </Button>
+          <div className='mb-2 flex items-center justify-center text-sm font-medium leading-7 text-[#999db1]'>
+            <hr className='' />
+            Atau Masuk <hr />
+          </div>
+
+          <Button
+            className='mb-3 ms-4 w-full border-2 border-[#6118e8] bg-transparent text-lg font-medium leading-7 text-[#6118e8]'
+            isLoading={processing}
+            onClick={submit}
+            startContent={<FaGoogle />}
+          >
+            Google
+          </Button>
+          <Link href={route('register')} className='mb-3 text-sm'>
+            <span className='text-[#777b8a] no-underline'>
+              Belum punya akun?
             </span>
-          </label>
-        </div>
-
-        <div className='mt-4 flex items-center justify-end'>
-          {canResetPassword && (
-            <Link
-              href={route('password.request')}
-              className='rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800'
-            >
-              Forgot your password?
-            </Link>
-          )}
-
-          <PrimaryButton className='ms-4' disabled={processing}>
-            Log in
-          </PrimaryButton>
+            <span className='text-[#6118e8] no-underline'> Daftar</span>
+          </Link>
         </div>
       </form>
     </GuestLayout>
